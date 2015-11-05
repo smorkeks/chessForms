e509b9a46936c0be57417c5cc7d40334b5311d11
Martin <marhu300@student.liu.se>
2015-11-05 11:02:09 +0100

    Changed name of Form to GUI. Added GUI functions.

---
+++ ChessForms/GUI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessForms
+{
+    public partial class GUI : Form
+    {
+        private string lastInput = "";
+
+        public GUI()
+        {
+            InitializeComponent();
+
+        }
+
+        private void GUI_Load(object sender, EventArgs e)
+        {
+            whiteAgentDropDown.SelectedIndex = 0;
+
+        }
+
+        public void putString(string s)
+        {
+            consoleOutput.Text += consoleInput.Text + "\n";
+        }
+
+        public string readString()
+        {
+            return lastInput;
+        }
+    }
+}
