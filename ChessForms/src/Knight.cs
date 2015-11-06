e819c2ae9a1c35cbddd2887d6c60f35bafdf715b
Alfred <alfzi942@student.liu.se>
2015-11-06 15:51:39 +0100

    Added new cover function for knight

--- ChessForms/src/Knight.cs
+++ ChessForms/src/Knight.cs
@@ -9,7 +9,7 @@ namespace ChessForms.src
     {
         public Knight(uint x, uint y, string col) : base(x, y, 3, col) { }
 
-        public override List<Tuple<uint, uint>> getPossibleMoves(Board.QueryFunc QF,uint turn)
+        public override List<Tuple<uint, uint>> getPossibleMoves(Board.QueryFunc QF, uint turn)
         {
 
             List<Tuple<uint, uint>> tmpList = new List<Tuple<uint, uint>>();
@@ -32,7 +32,7 @@ namespace ChessForms.src
                 {
                     Square S = QF((uint)(getX() + x), (uint)(getY() + y));
                     Piece P = S.getPiece();
-                    if (P != null) 
+                    if (P != null)
                     {
                         if (!isSameColour(P))
                         {
@@ -51,9 +51,28 @@ namespace ChessForms.src
 
         public override List<Tuple<uint, uint>> getCover(Board.QueryFunc QF)
         {
-            List<Tuple<uint, uint>> cover = new List<Tuple<uint, uint>>() { };
+            List<Tuple<uint, uint>> cover = new List<Tuple<uint, uint>>();
+            List<Tuple<int, int>> jmpList = new List<Tuple<int, int>>(); //Contains all possible jump locations
+            jmpList.Add(new Tuple<int, int>(1, 2));
+            jmpList.Add(new Tuple<int, int>(1, -2));
+            jmpList.Add(new Tuple<int, int>(2, 1));
+            jmpList.Add(new Tuple<int, int>(2, -1));
+            jmpList.Add(new Tuple<int, int>(-1, 2));
+            jmpList.Add(new Tuple<int, int>(-1, -2));
+            jmpList.Add(new Tuple<int, int>(-2, 1));
+            jmpList.Add(new Tuple<int, int>(-2, -1));
 
-            // TODO: Do stuff
+            // Loops over jump locations
+            foreach (Tuple<int, int> item in jmpList)
+            {
+                int x = item.Item1;
+                int y = item.Item2;
+                if (withinBoard((int)getX() + x, (int)getY() + y))
+                {
+                    
+                    cover.Add(new Tuple<uint, uint>((uint)(getX() + x), (uint)(getY() + y)));
+                }
+            }
 
             return cover;
         }
