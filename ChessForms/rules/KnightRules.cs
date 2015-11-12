6b0b32912174e8c123f93e3585bcab7355a16324
Martin <marhu300@student.liu.se>
2015-11-12 09:59:53 +0100

    Added more rule classes.

---
+++ ChessForms/rules/KnightRules.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessForms.src;
+
+namespace ChessForms.rules
+{
+    class KnightRules : PieceRules
+    {
+
+        //Contains all possible jump locations, relative to piece
+        private List<Tuple<int, int>> jmpList;
+
+        public KnightRules() : base()
+        {
+            jmpList = new List<Tuple<int, int>>();
+            jmpList.Add(new Tuple<int, int>(1, 2));
+            jmpList.Add(new Tuple<int, int>(1, -2));
+            jmpList.Add(new Tuple<int, int>(2, 1));
+            jmpList.Add(new Tuple<int, int>(2, -1));
+            jmpList.Add(new Tuple<int, int>(-1, 2));
+            jmpList.Add(new Tuple<int, int>(-1, -2));
+            jmpList.Add(new Tuple<int, int>(-2, 1));
+            jmpList.Add(new Tuple<int, int>(-2, -1));
+        }
+
+        public override List<Tuple<uint, uint>> getPossibleMoves(Board board, Piece piece)
+        {
+            List<Tuple<uint, uint>> moves = new List<Tuple<uint, uint>>();
+            int px = (int) piece.getX();
+            int py = (int) piece.getY();
+
+            // Loops over jump locations
+            foreach (Tuple<int, int> item in jmpList)
+            {
+                int jmpX = item.Item1 + px;
+                int jmpY = item.Item2 + py;
+                if (board.withinBoard(jmpX, jmpY))
+                {
+                    Piece P = board.getPieceAt((uint) jmpX, (uint) jmpY);
+                    if (P != null)
+                    {
+                        if (!piece.isSameColour(P))
+                        {
+                            moves.Add(new Tuple<uint, uint>((uint)jmpX, (uint)jmpY));
+                        }
+                    }
+                    else
+                    {
+                        moves.Add(new Tuple<uint, uint>((uint)jmpX, (uint)jmpY));
+                    }
+                }
+            }
+
+            // Filter for check situations
+            checkFilter(ref moves, board, piece);
+
+            return moves;
+        }
+
+        public override List<Tuple<uint, uint>> getCover(Board board, Piece piece)
+        {
+            List<Tuple<uint, uint>> cover = new List<Tuple<uint, uint>>();
+            int px = (int)piece.getX();
+            int py = (int)piece.getY();
+
+            // Loops over jump locations
+            foreach (Tuple<int, int> item in jmpList)
+            {
+                int jmpX = item.Item1 + px;
+                int jmpY = item.Item2 + py;
+                if (board.withinBoard(jmpX, jmpY))
+                {
+                    cover.Add(new Tuple<uint, uint>((uint)jmpX, (uint)jmpY));
+                }
+            }
+
+            return cover;
+        }
+    }
+}
---
+++ ChessForms/src/Move.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessForms.src
+{
+    class Move
+    {
+        // --- Flag for giving up ---
+        private bool giveUp;
+        
+        public bool GiveUp
+        {
+            get { return giveUp; }
+            set { giveUp = value; }
+        }
+
+        // --- Coordinates from and to ---
+        private int fromX, fromY, toX, toY;
+        
+        public int FromX
+        {
+            get { return fromX; }
+            set { fromX = value; }
+        }
+
+        public int FromY
+        {
+            get { return fromY; }
+            set { fromY = value; }
+        }
+
+        public int ToX
+        {
+            get { return toX; }
+            set { toX = value; }
+        }
+
+        public int ToY
+        {
+            get { return toY; }
+            set { toY = value; }
+        }
+
+        // --- Constructors ---
+
+        public Move()
+        {
+            fromX = 0;
+            fromY = 0;
+            toX = 0;
+            toY = 0;
+            giveUp = false;
+        }
+
+        public Move(int toX, int toY)
+        {
+            fromX = 0;
+            fromY = 0;
+            this.toX = toX;
+            this.toY = toY;
+            giveUp = false;
+        }
+
+        public Move(int fromX, int fromY, int toX, int toY)
+        {
+            this.fromX = fromX;
+            this.fromY = fromY;
+            this.toX = toX;
+            this.toY = toY;
+            giveUp = false;
+        }
+
+        // --- Other functions ---
+
+        public void setTo(int x, int y)
+        {
+            toX = x;
+            toY = y;
+        }
+
+        public void setFrom(int x, int y)
+        {
+            fromX = x;
+            fromY = y;
+        }
+
+    }
+}
