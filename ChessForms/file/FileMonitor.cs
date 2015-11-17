897b00fda0b50bba5f5c1b8208dd69b54ca1b4c3
Martin <marhu300@student.liu.se>
2015-11-17 10:05:41 +0100

    Added FileMonitor

---
+++ ChessForms/file/FileMonitor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ChessForms.file
+{
+    public class FileMonitor
+    {
+
+        private static string filePath = AppDomain.CurrentDomain.BaseDirectory + "../../saveFiles/current_state";
+        private delegate void OnChange(object source, FileSystemEventArgs e);
+
+        public FileMonitor(OnChange callback)
+        {
+            // Create a new FileSystemWatcher and set its properties.
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = filePath;
+            /* Watch for changes in LastAccess and LastWrite times, and
+               the renaming of files or directories. */
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            // Only watch text files.
+            watcher.Filter = "*.txt";
+
+            // Add event handlers.
+            watcher.Changed += new FileSystemEventHandler(callback);
+
+            // Begin watching.
+            watcher.EnableRaisingEvents = true;
+        }
+    }
+}
